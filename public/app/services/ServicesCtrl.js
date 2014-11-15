11c33b4507dc88445d5894d2b577fbd446d3282e
Russell Durham <rdurham1007@gmail.com>
2014-11-15 15:29:09 -0600

    Fixing primary character selection issue

--- public/app/services/ServicesCtrl.js
+++ public/app/services/ServicesCtrl.js
@@ -1,7 +1,10 @@
 angular.module('ssAuth').controller('ServicesCtrl', ['CharacterService', 'SessionService', '$scope', function(CharacterService, SessionService, $scope){
 
     SessionService.getCurrentUser().then(function(user){
-        $scope.primaryCharacter = user.character;
+        if(user.character && user.character.id)
+            $scope.primaryCharacter = user.character;
+        else
+            $scope.primaryCharacter = undefined;
     });
 
     CharacterService.getByUserId().then(function(characters){
